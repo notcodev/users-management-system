70b92637c403f9d4b6056efb03b5f2a984d71fe3
Erik <slmv_e@icloud.com>
2024-07-19 11:32:37 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite({
-      generatedRouteTree: './src/app/routeTree.gen.ts',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      react(),
+      TanStackRouterVite({
+        generatedRouteTree: './src/app/routeTree.gen.ts',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://404bf3641871.vps.myjino.ru',
+    server: {
+      proxy: {
+        '/api': process.env.VITE_API_URL as string,
+      },
     },
-  },
-})
+  })
+}
