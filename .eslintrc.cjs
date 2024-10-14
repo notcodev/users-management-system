7a206afa1e48cb1cc67e507a7a2eefc5718b76a2
Erik <slmv_e@icloud.com>
2024-10-15 01:41:01 +0300

    fixed eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,14 @@ const { configure, presets } = require('eslint-kit')
 
 module.exports = configure({
   allowDebug: process.env.NODE_ENV !== 'production',
-  parser: '@typescript-eslint/parser',
-  root: true,
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'routeTree.gen.ts'],
-  extends: ['plugin:@tanstack/eslint-plugin-query/recommended'],
+  extend: {
+    root: true,
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'routeTree.gen.ts'],
+    extends: ['plugin:@tanstack/eslint-plugin-query/recommended'],
+    parserOptions: {
+      project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    },
+  },
   presets: [
     presets.imports(),
     presets.node(),
